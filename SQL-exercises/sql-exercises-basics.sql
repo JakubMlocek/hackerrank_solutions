0be5550a4e9c3ecb7424cb3edfa34c0d8f703894
JakubMlocek <jakmlo@Jakubs-MacBook-Air.local>
2022-03-29 13:06:21 +0200

    new exercises added

--- SQL-exercises/sql-exercises-basics.sql
+++ SQL-exercises/sql-exercises-basics.sql
@@ -62,4 +62,38 @@ FROM STUDENTS
 WHERE Marks>75
 ORDER BY right(Name,3) ASC, ID ASC;
 
+/* Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order. */
+
+SELECT name
+FROM Employee
+ORDER BY name ASC
+
+/* Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id. */
+
+SELECT name
+FROM Employee
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id ASC
+
+/*Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to  decimal places.*/
+
+SELECT ROUND(SUM(LAT_N),4)
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345
+
+/* Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than . Truncate your answer to  decimal places. */
+
+SELECT ROUND(MAX(LAT_N),4)
+FROM STATION
+WHERE LAT_N < 137.2345
+
+/* Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places. */
+
+SELECT ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N = (
+    SELECT max(LAT_N)
+    FROM STATION
+    WHERE LAT_N < 137.2345
+)
 
--- SortingAlgorithms/counting_sort_2.py
+++ SortingAlgorithms/counting_sort_2.py
@@ -3,14 +3,15 @@ Often, when a list is sorted, the elements being sorted are just keys to other v
 The counting sort is used if you just need to sort a list of integers. Rather than using a comparison, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
 """
 
-
-
-
-def countingSort(arr, max_val):
-    n = len(arr)
-    C = [0 for _ in range(max_val)]
-
-    for num in arr:
-        C[num] += 1
-
-    return C
+def countingSort(A,k=100):
+    C = [0]*k
+    B = [0]*len(A)
+    for i in range(len(A)):
+        C[A[i]] += 1
+    for i in range(1, k):
+        C[i] += C[i-1]
+    for i in range(len(A)-1, -1, -1):
+        C[A[i]] -= 1
+        B[C[A[i]]] = A[i]
+    return B
+    
